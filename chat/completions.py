a292a303c39ecd11f6fd1481199f23c3189ef0ff
Leonard Excoffier <excoffier.leonard@gmail.com>
2024-02-06 22:30:31 -0500

    changed structure to prep for cli

--- chat/completions.py
+++ chat/completions.py
@@ -6,27 +6,45 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 completion = client.chat.completions.create(
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ],  
+  messages=[{"role": "user", "content": "Hello!"}],
   model="gpt-4",
-  frequency_penalty=0.0,
-  logit_bias={},
-  logprobs=False,
+  frequency_penalty=None,
+  logit_bias=None,
+  logprobs=None,
   top_logprobs=None,
   max_tokens=None,
-  n=1,
-  presence_penalty=0.0,
-  response_format={"type": "text"},
-  seed=0,
+  n=None,
+  presence_penalty=None,
+  response_format=None,
+  seed=None,
   stop=None,
-  stream=False,
-  temperature=1.0,
-  top_p=1.0,  
-  tools=[{"type": "function", "function": {"description": "description", "name": "name", "parameters": {}}}],
-  tool_choice="none",
-  user=""
+  stream=None,
+  temperature=None,
+  top_p=None,  
+  tools=None,
+  tool_choice=None,
+  user=None
 )
+
+'''
+messages=messages,
+model=model,
+frequency_penalty=frequency_penalty,
+logit_bias=logit_bias,
+logprobs=logprobs,
+top_logprobs=top_logprobs,
+max_tokens=max_tokens,
+n=n,
+presence_penalty=presence_penalty,
+response_format=response_format,
+seed=seed,
+stop=stop,
+stream=stream,
+temperature=temperature,
+top_p=top_p,
+tools=tools,
+tool_choice=tool_choice,
+user=user
+'''
 
 print(completion.choices[0].message.content)
